08ddc909ddf1800fbea03898b10297de52683cf8
Ana Rodriguez Reiner <arodriguezreiner@outlook.com>
2023-06-07 21:11:36 -0700

    commit lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+function saturdayFun (event = "roller-skate") {
+    return "This Saturday, I want to " + event + "!";
+}
+saturdayFun()
+
+function mondayWork (event = "go to the office") {
+    return "This Monday, I will " + event + "." ;
+}
+mondayWork()
+
+function wrapAdjective(accent = "*") {
+    return function(adjective = "special") {
+        return `You are ${accent}${adjective}${accent}!`;
+    }
+}
+wrapAdjective()(adjective);
+
